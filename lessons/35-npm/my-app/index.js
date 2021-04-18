b0cbc27fa86f053a46e128f05a86563443770c2e
Ilya <ilya.naumenko53@gmail.com>
2021-04-18 19:33:23 +0300

    hw npm

--- lessons/35-npm/my-app/index.js
+++ lessons/35-npm/my-app/index.js
@@ -1,4 +1,7 @@
 // import functions from your package
-const { smartSearch } = require('[NAME_OF_YOUR_PACKAGE]')
+const { smartSearch } = require("./node_modules/first-public-p1xel");
 
-console.log(smartSearch(['Smith', 'John'], 'John'))
+const { uniqArray } = require("first-public-p1xel");
+
+console.log(smartSearch(["Smith", "John"], "John"));
+console.log(uniqArray([1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8]));
--- lessons/35-npm/public-package/index.js
+++ lessons/35-npm/public-package/index.js
@@ -1,6 +1,8 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
+const uniqArray = (value) => new Set([...value]);
 
 module.exports = {
   smartSearch,
-}
+  uniqArray,
+};
